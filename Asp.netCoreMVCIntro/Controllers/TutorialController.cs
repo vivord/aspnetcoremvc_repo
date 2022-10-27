6a3faf404c29cc7f095f029918e5f8891da520a6
Vivord RITH <vivord.rith@gmail.com>
2022-10-27 13:23:46 +0700

    asp.net core mvc repository

---
+++ Asp.netCoreMVCIntro/Controllers/TutorialController.cs
@@ -0,0 +1,26 @@
+﻿using Asp.netCoreMVCIntro.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.netCoreMVCIntro.Controllers
+{
+
+    public class TutorialController : Controller
+    {
+        private readonly ITutorialRepository _tourRepository;
+        public TutorialController(ITutorialRepository tutorialRepository)
+        {
+            _tourRepository = tutorialRepository;
+        }
+        public IActionResult Index()
+        {
+            //var tutorials = new List<Tutorial>
+            //{
+            //    new Tutorial{Id =1, Name = "C#", Description = "C# Tutorial"},
+            //    new Tutorial{Id =2, Name = "Asp.net core", Description = "Asp.net core Tutorial"},
+            //};
+            //var tutorials = new TutorialRepository().GetAllTutorial();
+            var tutorials = _tourRepository.GetAllTutorial();
+            return View(tutorials);
+        }
+    }
+}
---
+++ Asp.netCoreMVCIntro/Repository/TutorialRepository.cs
@@ -0,0 +1,41 @@
+﻿using Asp.netCoreMVCIntro.Models;
+
+namespace Asp.netCoreMVCIntro.Repository
+{
+    public class TutorialRepository : ITutorialRepository
+    {
+        private readonly List<Tutorial> _tutorials;
+        public TutorialRepository()
+        {
+            _tutorials = new List<Tutorial>()
+            {
+                new Tutorial{Id =1, Name = "C#", Description = "C# Tutorial"},
+                new Tutorial{Id =2, Name = "Asp.net core", Description = "Asp.net core Tutorial"},
+            };
+        }
+        public Tutorial Add(Tutorial tutorial)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tutorial Delete(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Tutorial> GetAllTutorial()
+        {
+            return _tutorials;
+        }
+
+        public Tutorial GetTutorial(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tutorial Update(Tutorial tutorial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
